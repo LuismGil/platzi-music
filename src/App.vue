552767cfe101ff86a3bb0f2c3a382257b21f7718
LuisM <luism6977@gmail.com>
2021-06-03 17:43:03 -0300

    uploading files

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar canciones",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+
+      .container
+        p
+          small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks") 
+            | {{ t.name }} - {{ t.artists[0].name }}
+</template>
+
+<script>
+import trackService from './services/track.js';
+
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+
+  methods: {
+    search () {
+      if (!this.searchQuery) { return }
+      trackService.search(this.searchQuery)
+      .then(res => {
+        this.tracks = res.tracks.items
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+
+  .results {
+    margin-top: 50px;
+  }
+</style>
---
+++ src/services/config.js
@@ -0,0 +1,5 @@
+const configService = {
+  apiUrl: 'https://platzi-music-api.herokuapp.com'
+};
+
+export default configService;
